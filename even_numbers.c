26de37bc73082d1980a70214ec7e8f790acf1541
raebrown-ctrl <brownrae6@gmail.com>
2023-04-03 15:12:32 -0700

    undefined ref

--- even_numbers.c
+++ even_numbers.c
@@ -24,11 +24,11 @@ stack_t *new_Node(int n)
 }
 
 /**
- * _push - push item
+ * monty_push - push item
  * @stack: is a parameter
  * @line_number: is value
  */
-void _push(stack_t **stack, unsigned int line_number)
+void monty_push(stack_t **stack, unsigned int line_number)
 {
 	stack_t *new = NULL;
 	(void)line_number;
@@ -42,12 +42,12 @@ void _push(stack_t **stack, unsigned int line_number)
 }
 
 /**
- * _pall - print elements stack
+ * monty_pall - print elements stack
  * @stack: is a parameter
  * @n: is value
  * Return: nothing
  */
-void _pall(stack_t **stack, unsigned int n)
+void monty_pall(stack_t **stack, unsigned int n)
 {
 	stack_t *current = NULL;
 	(void)n;
@@ -59,5 +59,47 @@ void _pall(stack_t **stack, unsigned int n)
 		dprintf(STDOUT_FILENO, "%d\n", current->n);
 		current = current->next;
 	}
+}
+
+/**
+ * monty_pop - Removes the top value element of a stack_t linked list.
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
+ */
+void monty_pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *next = NULL;
+
+	if ((*stack)->next == NULL)
+	{
+		set_op_tok_error(pop_error(line_number));
+		return;
+	}
+
+	next = (*stack)->next->next;
+	free((*stack)->next);
+	if (next)
+		next->prev = *stack;
+	(*stack)->next = next;
+}
+
+/**
+ * monty_add - Adds the top two values of a stack_t linked list.
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
+ *
+ * Description: The result is stored in the second value node
+ *              from the top and the top value  is removed.
+ */
+void monty_add(stack_t **stack, unsigned int line_number)
+{
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+	{
+		set_op_tok_error(short_stack_error(line_number, "add"));
+		return;
+	}
+
+	(*stack)->next->next->n += (*stack)->next->n;
+	monty_pop(stack, line_number);
 }
 
